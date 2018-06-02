8a2f1dfef9234f7ae1377d13b09cfe7059c490aa
goodjwon <jwon76@naver.com>
2018-06-02 10:35:36 +0900

    npm, underscore, init ...

---
+++ underscore.js
@@ -0,0 +1,14 @@
+
+var _ = require('underscore');
+var arr = [5,3,2,3,,5,3,2,1];
+
+console.log(arr[0]);
+
+console.log(_.first(arr));
+
+console.log(_.last(arr));
+
+console.log(arr[arr.length-1]);
+
+
+
