4462657dea3725a54397811cea799598db556931
goodjwon <goodjwon@gmail.com>
2018-06-05 08:04:13 +0900

    정저, 동적 처리 방법

--- app.js
+++ app.js
@@ -5,8 +5,32 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('Hello home page');;
 });
+
+app.get('/dynamic', function(req, res){
+    var lis = '';
+    var time = Date();
+    for(var i=0; i < 5; i++){
+        lis = lis + '<li>coding</li>';
+    };
+
+    var body = `<html>
+                    <header>
+                        <meta>
+                        <title></title>
+                    </header>
+                    <body>
+                        Hello Dynamic!!
+                        <ul>
+                        ${lis}
+                        </ul>
+                        ${time}
+                    </body>
+                </html>`;
+    res.send(body);
+});
+
 app.get('/route', function(req, res){
-    res.send('Hello Router, <img src="/61986.jpg">')
+    res.send('Hello Router, <img src="/61986.jpg">');
 })
 app.get('/login', function(req, res){
     res.send('<h1>Login please</h1>');
