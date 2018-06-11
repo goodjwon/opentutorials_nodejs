30ed43690d090cc2226da344a13421834efee5fe
goodjwon <jwon76@naver.com>
2018-06-11 22:23:09 +0900

    파일 쓰기 기능 작성

---
+++ app_file.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.locals.pretty = true;
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/topic/new', function (req, res) {
+    res.render('new');
+
+})
+app.post('/topic', function (req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+
+    fs.writeFile('data/'+title, description, function (err) {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Susses!');
+    });
+
+
+
+
+
+})
+app.listen(3000, function () {
+    console.log('Conneted, 3000 port');
+})
