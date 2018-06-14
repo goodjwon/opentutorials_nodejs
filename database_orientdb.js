b04fee0d3a6ca534bc3ab06d46af07edc8d7217f
goodjwon <jwon76@naver.com>
2018-06-14 23:23:37 +0900

    db 연결 (오리엔트db)

---
+++ database_orientdb.js
@@ -0,0 +1,17 @@
+var OrientDB = require('orinetjs');
+
+var server = OrientDB({
+    localhost: 'localhost',
+    port: '2424',
+    username: 'root',
+    password: 'orientdb'
+});
+
+var db = server.use('o2');
+
+db.record.get('#26:0')
+.then(function (record) {
+
+    console.log('Loaded recode:', record);
+
+});
