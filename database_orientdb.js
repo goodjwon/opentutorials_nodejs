4f22665bc91683f51d664ecb59cdf69099cf6d12
goodjwon <goodjwon@gmail.com>
2018-06-20 07:53:23 +0900

    conneted orientdb success

--- database_orientdb.js
+++ database_orientdb.js
@@ -1,15 +1,15 @@
-var OrientDB = require('orinetjs');
+var OrientDB = require('orientjs');
 
 var server = OrientDB({
     localhost: 'localhost',
     port: '2424',
     username: 'root',
-    password: 'eoqkr'
+    password: 'eoqkr1031'
 });
 
 var db = server.use('o2');
 
-db.record.get('#26:0')
+db.record.get('#21:0')
 .then(function (record) {
 
     console.log('Loaded recode:', record);
