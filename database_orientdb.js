2a3353936e5cd41f9e28ae8f46543eb38166024a
goodjwon <goodjwon@gmail.com>
2018-06-21 07:37:21 +0900

    orientdb select where

--- database_orientdb.js
+++ database_orientdb.js
@@ -9,9 +9,26 @@ var server = OrientDB({
 
 var db = server.use('o2');
 
+/**
 db.record.get('#21:0')
 .then(function (record) {
-
     console.log('Loaded recode:', record);
-
 });
+
+let sql = 'SELECT FROM topic';
+db.query(sql).then(function(results){
+    console.log(results);
+});
+ */
+
+var sql = 'SELECT FROM topic WHERE @rid=:rid';
+var param = {
+        params: {
+            rid: '#21:0'
+        }
+    };
+
+db.query(sql, param).then(function(results){
+    console.log(results);
+});
+
